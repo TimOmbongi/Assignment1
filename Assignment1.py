61ad9a83c1d3946da1c9c6933ca7f7f5dbfdb151
TimOmbongi <tombongi@students.kennesaw.edu>
2026-06-12 20:01:01 -0400

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -3,23 +3,26 @@
 # Student Name: Tim Ombongi
 # Assignment Number: Assignment 1
 # Due Date: 06/11/2026
-# Purpose: This Program allows the user to input buffer and choose to display them, clear them, or store them. When the user is done they can exit the program.
+# Purpose: This Program allows the user to input buffer and choose to display them, clear them, or store them. When the user is done, they can exit the program.
 # Pycharm
+
 buffer = ""
 choice = ""
-
+#Variables for buffers and the choices they make
 while choice != "4":
+#Creating a loop until break
 
     print("Menu")
     print("1. Append data to the input buffer")
     print("2. Clear the input buffer")
     print("3. Display the input buffer")
     print("4. Exit the program")
+    #Creating a menu option
 
     choice = input("Enter your choice: ")
 
     match choice:
-
+    #Matching inputs with choices and invalid inputs.
         case "1":
             text = input("Enter text: ")
             buffer += text
@@ -35,6 +38,7 @@ while choice != "4":
 
         case "4":
             print("exiting program..")
+            break
 
         case _:
             print("Invalid input.")
